6e27c816481cea8da1b661f0aee194e592644587
Sherry D <trilliun@users.noreply.github.com>
2016-08-29 19:51:13 -0400

    add water tile

--- src/data/TileType.java
+++ src/data/TileType.java
@@ -2,7 +2,7 @@ package data;
 
 public enum TileType {
 	
-	Grass("grass64", true), Dirt("dirt64", false);
+	Grass("grass64", true), Dirt("dirt64", false), Water("water64", false);
 	
 	String textureName;
 	boolean buildable;
