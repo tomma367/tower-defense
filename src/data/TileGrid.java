7d3a45aa4125ea41769af1001f30229556a106d1
Sherry D <trilliun@users.noreply.github.com>
2016-08-29 19:51:54 -0400

    add map generation

---
+++ src/data/TileGrid.java
@@ -0,0 +1,46 @@
+package data;
+
+public class TileGrid {
+
+	public Tile[][] map;
+	
+	public TileGrid() {
+		map = new Tile[20][15];
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				map [i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Grass);
+			}
+		}
+	}
+	
+	public TileGrid(int[][] newMap) {
+		map = new Tile[20][15];
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				switch (newMap[j][i]) {
+				case 0:
+					map [i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Grass);
+					break;
+				case 1:
+					map [i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Dirt);
+					break;
+				case 2:
+					map [i][j] = new Tile(i * 64, j * 64, 64, 64, TileType.Water);
+					break;
+				}
+			}
+		}
+	}
+	
+	
+	public void Draw() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				Tile t = map[i][j];
+				t.Draw(); 
+			}
+		}
+	}
+}
